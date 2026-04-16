fe8538f296be0192eef5b09289f9e46cdcd03903
Charlotte <smit-charlotte@outlook.com>
2026-04-16 15:18:42 +0200

    working on the rectangles

---
+++ src/components/Date.jsx
@@ -0,0 +1,13 @@
+import './datebar.css'
+
+export default function Date({ date }) {
+
+    return (
+
+        <>
+        <div className="datebar">
+            <p>{date}</p>
+        </div>
+        </>
+    )
+}
---
+++ src/components/Rectangle.jsx
@@ -0,0 +1,26 @@
+import './rectangle.css'
+
+export default function Rectangle({ titre, lieu, type }) {
+    // prendre en props le duo de couleurs, le titre de l'event, le lieu, type d'event
+    // pour les couleurs de chaque rectangle, elle serait générée selon un ordre : par exemple, en ayant un set de 3 duos de couleurs, ces couleurs seraient appliquées dans l'ordre. 
+
+    return(
+        <>
+            <div className="rectangle">
+                <div className="left">
+                    <h2>
+                        {titre}
+                    </h2>
+                </div>
+                <div className="right">
+                    <h3>
+                        {lieu}
+                    </h3>
+                    <h4>
+                        {type}
+                    </h4>
+                </div>
+            </div>
+        </>
+    )
+}
