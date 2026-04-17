86f2ae4741ba38daf47927a197d0724220c50ab1
Charlotte <smit-charlotte@outlook.com>
2026-04-17 14:12:18 +0200

    change of fonts

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ function App() {
     <SecondBar /> */}
     {/* ici, les infos our chaque event sont hardcoded. elles viendront d'une database via mysql dans la version finale. */}
     <Date date={"16 avril 2026"} />
-    <Rectangle titre={"Bozar Bassment #7: Crystallmess (live) - Akano Toto (live)"} lieu={"Bozar"} type={"concert"} />
+    <Rectangle titre={"Bozar Bassment #7: Crystallmess (live) - Akano Toto (live)"} lieu={"Bozar"} type={"concert"} type2={"musique"} couleur={"vert"} />
+        <Rectangle titre={"Forward presents: Buda BXL"} lieu={"Buda BXL"} type={"clubbing"} type2={"musique"} couleur={"blue"} />
+
     </>
   )
 }
--- src/components/Rectangle.jsx
+++ src/components/Rectangle.jsx
@@ -1,12 +1,12 @@
 import './rectangle.css'
 
-export default function Rectangle({ titre, lieu, type }) {
+export default function Rectangle({ titre, lieu, type, type2, couleur }) {
     // prendre en props le duo de couleurs, le titre de l'event, le lieu, type d'event
     // pour les couleurs de chaque rectangle, elle serait générée selon un ordre : par exemple, en ayant un set de 3 duos de couleurs, ces couleurs seraient appliquées dans l'ordre. 
 
     return(
         <>
-            <div className="rectangle vert">
+            <div className={`rectangle ${couleur}`}>
                 <div className="left">
                     <h2 className="title">
                         {titre}
@@ -16,9 +16,14 @@ export default function Rectangle({ titre, lieu, type }) {
                     <h3>
                         {lieu}
                     </h3>
-                    <h4>
-                        {type}
-                    </h4>
+                    <div className="tags">
+                        <h4>
+                            {type}
+                        </h4>
+                        <h4>{type2}
+
+                        </h4>
+                    </div>
                 </div>
             </div>
         </>
