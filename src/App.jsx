6b977faf98b1da735bc5fcfd1c3c3e6d2cbf8e9a
Charlotte <smit-charlotte@outlook.com>
2026-04-20 22:44:18 +0200

    new color yellow

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ function App() {
     <Rectangle titre={"Forward presents: Buda BXL"} lieu={"Buda BXL"} type={"clubbing"} type2={"musique"} couleur={"blue"} />
     <Rectangle titre={"Fuse presents: For Your Entertainment tour with VTSS (A/V)"} lieu={"Fuse"} type={"clubbing"} type2={"musique"} couleur={"purple"} />
     <Date date={"17 avril 2026"} />
-    <Rectangle titre={"Bozar Bassment #7: Crystallmess (live) - Akano Toto (live)"} lieu={"Bozar"} type={"concert"} type2={"musique"} couleur={"vert"} />
-    <Rectangle titre={"Forward presents: Buda BXL"} lieu={"Buda BXL"} type={"clubbing"} type2={"musique"} couleur={"blue"} />
-    <Rectangle titre={"Fuse presents: For Your Entertainment tour with VTSS (A/V)"} lieu={"Fuse"} type={"clubbing"} type2={"musique"} couleur={"purple"} />
+    <Rectangle titre={"gl1tch.IV"} lieu={"La Fabriek"} type={"clubbing"} type2={"musique"} couleur={"yellow"} />
+    <Rectangle titre={"Collectif Méta pres. MÉTA 3 YEARS w/ Eduardo De La Calle"} lieu={"Bodies in Space"} type={"clubbing"} type2={"musique"} couleur={"blue"} />
+    <Rectangle titre={"Seefeel"} lieu={"AB Club"} type={"concert"} type2={"musique"} couleur={"purple"} />
     </>
   )
 }
